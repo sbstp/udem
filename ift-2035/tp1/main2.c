32dddf5e327e78228881ebbbb3beb3ed8c93a4e8
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-09-30 16:19:11 -0400

    More work on the second version

--- ift-2035/tp1/main2.c
+++ ift-2035/tp1/main2.c
@@ -5,7 +5,7 @@
 typedef enum { false, true } bool;
 
 struct digit {
-    char val;
+    int val;
     struct digit *next;
 };
 
@@ -15,11 +15,31 @@ struct num {
     struct digit *first;
 };
 
-struct num* num_new(const char*);
-void num_destroy(struct num*);
-char* num_to_string(struct num*);
+/* convertit une valeur numérique [0-9] en son caractère */
+char val_to_car(char);
+/* convertit un caractère ['0'-'9'] en sa valeur numérique */
+char car_to_val(char);
 
-struct num* num_new(const char *text) {
+/* créer un nombre à partir d'une string */
+struct num* num_from_str(const char*);
+/* créer un clone complet du nombre */
+struct num* num_clone(const struct num*);
+/* libérer la mémoire utilisée par un nombre */
+void num_free(struct num*);
+/* formatter le nombre en string */
+char* num_to_str(const struct num*);
+
+/* implémentation */
+
+inline char val_to_car(char val) {
+    return val + '0';
+}
+
+inline char car_to_val(char car) {
+    return car - '0';
+}
+
+struct num* num_from_str(const char *text) {
     int len, i, lo;
     struct num *n;
     struct digit *p, *d;
@@ -30,13 +50,21 @@ struct num* num_new(const char *text) {
     n = malloc(sizeof(struct num));
     if (n == NULL) return NULL;
 
+    /* si le nombre débute par '-', il est négatif */
     n->pos = text[0] != '-';
     n->len = 0;
+    /* si il y a un signe '-', on arrête de copier avant 1 caractère plus tôt */
     lo = !n->pos;
 
     for (i = len - 1; i >= lo; i--) {
         d = malloc(sizeof(struct digit));
-        d->val = text[i];
+        /* si on arrive pas à allouer, on détruit tout ce qui a été alloué auparavant */
+        if (d == NULL) {
+            num_free(n);
+            return NULL;
+        }
+
+        d->val = car_to_val(text[i]);
         n->len++;
         if (p == NULL)
             n->first = p = d;
@@ -48,7 +76,41 @@ struct num* num_new(const char *text) {
     return n;
 }
 
-void num_destroy(struct num *n) {
+struct num* num_clone(const struct num *n) {
+    struct num *c;
+    struct digit *d, *prev, *new;
+
+    c = malloc(sizeof(struct num));
+    if (c == NULL) return NULL;
+
+    c->pos = n->pos;
+    c->len = n->len;
+
+    d = n->first;
+    prev = NULL;
+    while (d != NULL) {
+        new = malloc(sizeof(struct digit));
+        if (new == NULL) {
+            num_free(c);
+            return NULL;
+        }
+
+        new->val = d->val;
+        /* si c'est la première itération, on attache new à c->first */
+        if (prev == NULL)
+            c->first = new;
+        /* sinon on attache new, au précédent */
+        else
+            prev->next = new;
+
+        prev = new;
+        d = d->next;
+    }
+
+    return c;
+}
+
+void num_free(struct num *n) {
     struct digit *d, *t;
 
     d = n->first;
@@ -61,7 +123,7 @@ void num_destroy(struct num *n) {
     free(n);
 }
 
-char* num_to_string(struct num *n) {
+char* num_to_str(const struct num *n) {
     int len, i;
     char *text;
     struct digit *d;
@@ -75,11 +137,11 @@ char* num_to_string(struct num *n) {
     i = len - 2;
     d = n->first;
     while (d != NULL) {
-        text[i--] = d->val;
+        text[i--] = val_to_car(d->val);
         d = d->next;
     }
-    text[len - 1] = '\0';
 
+    text[len - 1] = '\0';
     if (!n->pos) {
         text[0] = '-';
     }
@@ -87,9 +149,10 @@ char* num_to_string(struct num *n) {
     return text;
 }
 
-int main() {
-    struct num *n = num_new("123456");
-    printf("%s\n", num_to_string(n));
-    num_destroy(n);
+int main(int argc, char **argv) {
+    struct num *n = num_from_str("123456");
+    struct num *c = num_clone(n);
+    printf("%d %d %s\n", n->pos, n->len, num_to_str(n));
+    printf("%d %d %s\n", c->pos, c->len, num_to_str(c));
     return 0;
 }
