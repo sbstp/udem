c17a1f5696531e12e1e70745dae0c5274c43563f
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-10-01 09:19:24 -0400

    Re-organize ast & add ast_node_free

--- ift-2035/tp1/main2.c
+++ ift-2035/tp1/main2.c
@@ -27,6 +27,48 @@ struct stack {
     void** items;
 };
 
+enum ast_node_kind {
+    AST_NODE_KIND_ASSIGN,
+    AST_NODE_KIND_USE,
+    AST_NODE_KIND_NUM,
+    AST_NODE_KIND_OPERATOR,
+};
+
+enum ast_oper_kind {
+    AST_OPER_KIND_ADD,
+    AST_OPER_KIND_SUB,
+    AST_OPER_KIND_MUL,
+};
+
+struct ast_node_assign {
+    char var;
+    struct ast_node* val;
+};
+
+struct ast_node_use {
+    char var;
+};
+
+struct ast_node_num {
+    int val; /* TODO: bignum */
+};
+
+struct ast_node_oper {
+    enum ast_oper_kind kind;
+    struct ast_node *op1;
+    struct ast_node *op2;
+};
+
+struct ast_node {
+    enum ast_node_kind kind;
+    union {
+        struct ast_node_assign *assign;
+        struct ast_node_use *use;
+        struct ast_node_num *num;
+        struct ast_node_oper *oper;
+    };
+};
+
 /* convertit une valeur numérique [0-9] en son caractère */
 char val_to_car(char);
 /* convertit un caractère ['0'-'9'] en sa valeur numérique */
@@ -64,6 +106,19 @@ bool stack_push(struct stack*, void*);
 void* stack_pop(struct stack*);
 /* libérer la mémoire utilisée par le tas */
 void stack_free(struct stack*);
+
+/* créer un noeud de type assignation */
+struct ast_node* ast_node_assign(char, struct ast_node*);
+/* créer un noeud de type utilisation de variable */
+struct ast_node* ast_node_use(char);
+/* créer un noeud de type nombre */
+struct ast_node* ast_node_num(const char*, int);
+/* créer un noeud de type opérateur */
+struct ast_node* ast_node_oper(enum ast_oper_kind, struct ast_node*, struct ast_node*);
+/* analyser la chaîne de caractères et construire l'ASA */
+struct ast_node* ast_parse(const char*);
+/* libérer la mémoire utilisée par l'ASA */
+void ast_node_free(struct ast_node*);
 
 /* implémentation */
 
@@ -292,51 +347,14 @@ void stack_free(struct stack *s) {
     free(s);
 }
 
-enum ast_kind {
-    AST_KIND_ASSIGN,
-    AST_KIND_USE,
-    AST_KIND_NUMBER,
-    AST_KIND_OPERATOR,
-};
-
-enum ast_oper_kind {
-    AST_OPER_KIND_ADD,
-    AST_OPER_KIND_SUB,
-    AST_OPER_KIND_MUL,
-};
-
-struct ast_node {
-    enum ast_kind kind;
-    union {
-        /* assignation de variable */
-        struct {
-            char var;
-            struct ast_node* val;
-        } assign;
-        /* utilisation de variable */
-        struct {
-            char var;
-        } use;
-        /* nombre */
-        struct {
-            char* val;
-        } num;
-        /* opérateur */
-        struct {
-            enum ast_oper_kind kind;
-            struct ast_node *op1;
-            struct ast_node *op2;
-        } oper;
-    };
-};
-
 struct ast_node* ast_node_assign(char var, struct ast_node *val) {
     struct ast_node *node;
 
     node = malloc(sizeof(struct ast_node));
-    node->kind = AST_KIND_ASSIGN;
-    node->assign.var = var;
-    node->assign.val = val;
+    node->kind = AST_NODE_KIND_ASSIGN;
+    node->assign = malloc(sizeof(struct ast_node_assign));
+    node->assign->var = var;
+    node->assign->val = val;
 
     return node;
 }
@@ -345,22 +363,20 @@ struct ast_node* ast_node_use(char var) {
     struct ast_node *node;
 
     node = malloc(sizeof(struct ast_node));
-    node->kind = AST_KIND_USE;
-    node->assign.var = var;
+    node->kind = AST_NODE_KIND_USE;
+    node->use = malloc(sizeof(struct ast_node_use));
+    node->use->var = var;
 
     return node;
 }
 
 struct ast_node* ast_node_num(const char* source, int len) {
     struct ast_node *node;
-    char* val;
-
-    val = malloc(sizeof(char) * len + 1);
-    strcpy(val, source);
 
     node = malloc(sizeof(struct ast_node));
-    node->kind = AST_KIND_NUMBER;
-    node->num.val = val;
+    node->kind = AST_NODE_KIND_NUM;
+    node->use = malloc(sizeof(struct ast_node));
+    node->num->val = atoi(source);
 
     return node;
 }
@@ -369,10 +385,11 @@ struct ast_node* ast_node_oper(enum ast_oper_kind kind, struct ast_node* op1, st
     struct ast_node *node;
 
     node = malloc(sizeof(struct ast_node));
-    node->kind = AST_KIND_OPERATOR;
-    node->oper.kind = kind;
-    node->oper.op1 = op1;
-    node->oper.op2 = op2;
+    node->kind = AST_NODE_KIND_OPERATOR;
+    node->oper = malloc(sizeof(struct ast_node_oper));
+    node->oper->kind = kind;
+    node->oper->op1 = op1;
+    node->oper->op2 = op2;
 
     return node;
 }
@@ -443,6 +460,27 @@ struct ast_node* ast_parse(const char* text) {
     return stack_pop(nodes);
 }
 
+void ast_node_free(struct ast_node *node) {
+    switch (node->kind) {
+        case AST_NODE_KIND_ASSIGN:
+            ast_node_free(node->assign->val);
+            free(node->assign);
+            break;
+        case AST_NODE_KIND_USE:
+            free(node->use);
+            break;
+        case AST_NODE_KIND_NUM:
+            free(node->num);
+            break;
+        case AST_NODE_KIND_OPERATOR:
+            ast_node_free(node->oper->op1);
+            ast_node_free(node->oper->op2);
+            free(node->oper);
+            break;
+    }
+    free(node);
+}
+
 struct inter {
     int vars[26];
 };
@@ -451,28 +489,27 @@ int inter_eval(struct inter *vm, struct ast_node *node) {
     int val;
 
     switch (node->kind) {
-        case AST_KIND_ASSIGN:
+        case AST_NODE_KIND_ASSIGN:
             printf("in assign\n");
-            val = inter_eval(vm, node->assign.val);
-            vm->vars[node->assign.var - 'a'] = val;
+            val = inter_eval(vm, node->assign->val);
+            vm->vars[node->assign->var - 'a'] = val;
             return val;
-        case AST_KIND_USE:
+        case AST_NODE_KIND_USE:
             printf("in use\n");
-            return vm->vars[node->use.var - 'a'];
-        case AST_KIND_NUMBER:
+            return vm->vars[node->use->var - 'a'];
+        case AST_NODE_KIND_NUM:
             printf("in number\n");
-            int val = atoi(node->num.val);
-            return val;
-        case AST_KIND_OPERATOR:
+            return node->num->val;
+        case AST_NODE_KIND_OPERATOR:
             printf("in oper\n");
-            switch (node->oper.kind) {
+            switch (node->oper->kind) {
                 // TODO: use big num
                 case AST_OPER_KIND_ADD:
-                    return inter_eval(vm, node->oper.op1) + inter_eval(vm, node->oper.op2);
+                    return inter_eval(vm, node->oper->op1) + inter_eval(vm, node->oper->op2);
                 case AST_OPER_KIND_SUB:
-                    return inter_eval(vm, node->oper.op2) - inter_eval(vm, node->oper.op1);
+                    return inter_eval(vm, node->oper->op2) - inter_eval(vm, node->oper->op1);
                 case AST_OPER_KIND_MUL:
-                    return inter_eval(vm, node->oper.op1) * inter_eval(vm, node->oper.op2);
+                    return inter_eval(vm, node->oper->op1) * inter_eval(vm, node->oper->op2);
             }
     }
 
@@ -493,16 +530,17 @@ int main(int argc, char **argv) {
     struct charbuff* cb;
 
     memset(&vm, 0, sizeof(struct inter));
-    cb = charbuff_new(20);
+    cb = charbuff_new(32);
 
     printf("> ");
     while ((c = getchar()) != EOF) {
         if (c == '\r') continue;
         if (c == '\n') {
             node = ast_parse(cb->buff);
-            val = inter_eval(&vm, ast_parse(cb->buff));
+            val = inter_eval(&vm, node);
             printf("%d\n", val);
             charbuff_clear(cb);
+            ast_node_free(node);
             printf("> ");
         } else {
             charbuff_push(cb, c);
