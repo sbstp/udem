16d9a7e5b1e5e479065ca91393ce69e19da2e516
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-10-01 20:18:42 -0400

    Error handling

--- ift-2035/tp1/main2.c
+++ ift-2035/tp1/main2.c
@@ -43,13 +43,23 @@ enum ast_node_kind {
     AST_NODE_KIND_ASSIGN,
     AST_NODE_KIND_USE,
     AST_NODE_KIND_NUM,
-    AST_NODE_KIND_OPERATOR,
+    AST_NODE_KIND_OPER,
 };
 
 enum ast_oper_kind {
     AST_OPER_KIND_ADD,
     AST_OPER_KIND_SUB,
     AST_OPER_KIND_MUL,
+};
+
+enum ast_parse_err {
+    AST_PARSE_ERR_OK,
+    AST_PARSE_ERR_ALLOC,
+    AST_PARSE_ERR_VARNAME,
+    AST_PARSE_ERR_MISSING_VARNAME,
+    AST_PARSE_ERR_TOKEN,
+    AST_PARSE_ERR_TOO_MANY_EXPR,
+    AST_PARSE_ERR_TOO_LITTLE_OPER,
 };
 
 struct ast_node_assign {
@@ -79,6 +89,11 @@ struct ast_node {
         struct ast_node_num *num;
         struct ast_node_oper *oper;
     };
+};
+
+struct ast_parse_result {
+    enum ast_parse_err err;
+    struct ast_node *node;
 };
 
 struct inter {
@@ -135,11 +150,13 @@ struct ast_node* ast_node_assign(char, struct ast_node*);
 /* créer un noeud de type utilisation de variable */
 struct ast_node* ast_node_use(char);
 /* créer un noeud de type nombre */
-struct ast_node* ast_node_num(const char*, int);
+struct ast_node* ast_node_num(const char*);
 /* créer un noeud de type opérateur */
 struct ast_node* ast_node_oper(enum ast_oper_kind, struct ast_node*, struct ast_node*);
 /* analyser la chaîne de caractères et construire l'ASA */
-struct ast_node* ast_parse(const char*);
+struct ast_parse_result ast_parse(const char*);
+/* obtenir un approprié message pour l'erreur donnée */
+const char* ast_parse_err_msg(enum ast_parse_err);
 /* libérer la mémoire utilisée par l'ASA */
 void ast_node_free(struct ast_node*);
 
@@ -389,22 +406,24 @@ struct tokenizer* tokenizer_new(const char *src) {
 
 bool tokenizer_next(struct tokenizer* tkzer, struct token *tok) {
     char car;
+    bool found_tok;
 
     if (tkzer->pos >= tkzer->len) return false;
 
+    found_tok = false;
     charbuff_clear(tkzer->cb);
     do {
         car = tkzer->src[tkzer->pos++];
         if (car == '\0') break;
         if (is_whitespace(car)) {
-            /* consommer les espaces superflus */
-            while (car != '\0' && is_whitespace(car)) {
-                car = tkzer->src[++tkzer->pos];
-            }
-            break;
+            if (found_tok) break;
+            else continue;
         }
+        found_tok = true;
         charbuff_push(tkzer->cb, car);
     } while (true);
+
+    if (!found_tok) return false;
 
     tok->len = tkzer->cb->len;
     tok->text = tkzer->cb->buff;
@@ -440,7 +459,7 @@ struct ast_node* ast_node_use(char var) {
     return node;
 }
 
-struct ast_node* ast_node_num(const char* source, int len) {
+struct ast_node* ast_node_num(const char* source) {
     struct ast_node *node;
 
     node = malloc(sizeof(struct ast_node));
@@ -455,7 +474,7 @@ struct ast_node* ast_node_oper(enum ast_oper_kind kind, struct ast_node* op1, st
     struct ast_node *node;
 
     node = malloc(sizeof(struct ast_node));
-    node->kind = AST_NODE_KIND_OPERATOR;
+    node->kind = AST_NODE_KIND_OPER;
     node->oper = malloc(sizeof(struct ast_node_oper));
     node->oper->kind = kind;
     node->oper->op1 = op1;
@@ -464,70 +483,131 @@ struct ast_node* ast_node_oper(enum ast_oper_kind kind, struct ast_node* op1, st
     return node;
 }
 
-struct ast_node* ast_parse(const char* text) {
-    int i, len;
-    char car, var;
-    struct ast_node *node, *op1, *op2;
+struct ast_parse_result ast_parse(const char* text) {
+    char car;
+    enum ast_oper_kind okind;
+    struct ast_parse_result result;
+    struct ast_node *node, *new, *op1, *op2;
     struct stack *nodes;
-    struct charbuff *cb;
+    struct tokenizer *tkzer;
+    struct token tok;
 
-    len = strlen(text);
-    nodes = stack_new(2);
-    cb = charbuff_new(16);
+    /* TODO: malloc errors */
+    nodes = stack_new(4);
+    tkzer = tokenizer_new(text);
+    result.err = AST_PARSE_ERR_OK;
 
-    for (i = 0; i < len; i++) {
-        car = text[i];
-        if (is_whitespace(car)) {
-            printf("whitespace\n");
-            continue;
-        }
-        if (is_digit(car)) {
-            while (is_digit(text[i]))
-                charbuff_push(cb, text[i++]);
-            printf("pushing number: %s\n", cb->buff);
-            node = ast_node_num(cb->buff, cb->len);
-            stack_push(nodes, node);
-        } else if (car == '+') {
-            op1 = stack_pop(nodes);
-            op2 = stack_pop(nodes);
-            // TODO: error if op1 or op2 null
-            printf("pushing add\n");
-            node = ast_node_oper(AST_OPER_KIND_ADD, op1, op2);
-            stack_push(nodes, node);
-        } else if (car == '-') {
-            op1 = stack_pop(nodes);
-            op2 = stack_pop(nodes);
-            // TODO: error if op1 or op2 null
-            printf("pushing sub\n");
-            node = ast_node_oper(AST_OPER_KIND_SUB, op1, op2);
-            stack_push(nodes, node);
-        } else if (car == '*') {
-            op1 = stack_pop(nodes);
-            op2 = stack_pop(nodes);
-            // TODO: error if op1 or op2 null
-            printf("pushing mul\n");
-            node = ast_node_oper(AST_OPER_KIND_MUL, op1, op2);
-            stack_push(nodes, node);
-        } else if (car == '=') {
-            var = text[++i];
-            if (is_letter(var)) {
-                node = stack_pop(nodes);
-                node = ast_node_assign(var, node);
-                stack_push(nodes, node);
-                printf("pushing assign of %c\n", var);
+    while (tokenizer_next(tkzer, &tok)) {
+        car = tok.text[0];
+        if (car == '=') {
+            /* assign */
+            node = stack_pop(nodes);
+
+            /* il n'y a rien à assigner */
+            if (node == NULL) {
+                result.err = AST_PARSE_ERR_TOO_LITTLE_OPER;
+                break;
             }
-            // TODO: invalid variable name
-        } else if (is_letter(car)) {
-            node = ast_node_use(car);
-            stack_push(nodes, node);
-            printf("pushing variable %c\n", car);
-        }
+            /* nom de variable manquant */
+            if (tok.len == 1) {
+                result.err = AST_PARSE_ERR_MISSING_VARNAME;
+                break;
+            }
+            /* le nom de variable est trop long */
+            if (tok.len > 2) {
+                result.err = AST_PARSE_ERR_VARNAME;
+                break;
+            }
 
-        charbuff_clear(cb);
+            new = ast_node_assign(tok.text[1], node);
+            stack_push(nodes, new);
+        } else if (is_letter(car)) {
+            /* use */
+            if (tok.len > 1) {
+                result.err = AST_PARSE_ERR_VARNAME;
+                break;
+            }
+            new = ast_node_use(car);
+            stack_push(nodes, new);
+        } else if (is_digit(car)) {
+            /* num */
+            /* TODO validate digit */
+            new = ast_node_num(tok.text);
+            stack_push(nodes, new);
+        } else if (car == '+' || car == '-' || car == '*') {
+            /* obtenir la deuxieme opérande, erreur si il n'y en a pas */
+            op2 = stack_pop(nodes);
+            if (op2 == NULL) {
+                result.err = AST_PARSE_ERR_TOO_LITTLE_OPER;
+                break;
+            }
+            /* obtenir la première opérande, erreur si il n'y en a pas */
+            op1 = stack_pop(nodes);
+            if (op1 == NULL) {
+                free(op2);
+                result.err = AST_PARSE_ERR_TOO_LITTLE_OPER;
+                break;
+            }
+            /* détecter l'opérateur */
+            switch (car) {
+                case '+':
+                    okind = AST_OPER_KIND_ADD;
+                    break;
+                case '-':
+                    okind = AST_OPER_KIND_SUB;
+                    break;
+                case '*':
+                    okind = AST_OPER_KIND_MUL;
+                    break;
+            }
+
+            new = ast_node_oper(okind, op1, op2);
+            stack_push(nodes, new);
+        } else {
+            result.err = AST_PARSE_ERR_TOKEN;
+            break;
+        }
     }
 
-    charbuff_free(cb);
-    return stack_pop(nodes);
+    /* si il reste plus qu'un item dans le stack, c'est qu'il y a plusieurs expressions */
+    if (nodes->len > 1) {
+        result.err = AST_PARSE_ERR_TOO_MANY_EXPR;
+    }
+
+    /* pas d'erreurs */
+    if (result.err == AST_PARSE_ERR_OK) {
+        result.node = stack_pop(nodes);
+    } else {
+        /* si il y a eu une erreur, on libère les noeuds restants */
+        while ((node = stack_pop(nodes)) != NULL) {
+            ast_node_free(node);
+        }
+    }
+
+    stack_free(nodes);
+    tokenizer_free(tkzer);
+    return result;
+}
+
+const char* ast_parse_err_msg(enum ast_parse_err err) {
+    switch (err) {
+        case AST_PARSE_ERR_OK:
+            return NULL;
+        case AST_PARSE_ERR_ALLOC:
+            return "Impossible d'allouer la mémoire nécessaire.";
+        case AST_PARSE_ERR_VARNAME:
+            return "Les noms de variables doivent être d'un seul caractère entre a et z.";
+        case AST_PARSE_ERR_MISSING_VARNAME:
+            return "Nom de variable manquant dans l'assignation.";
+        case AST_PARSE_ERR_TOKEN:
+            return "La ligne contient un caractère invalide.";
+        case AST_PARSE_ERR_TOO_MANY_EXPR:
+            return "La ligne entrée contient plus qu'une expression.";
+        case AST_PARSE_ERR_TOO_LITTLE_OPER:
+            return "L'opérateur nécessite plus d'opérandes.";
+    }
+
+    return NULL;
 }
 
 void ast_node_free(struct ast_node *node) {
@@ -542,7 +622,7 @@ void ast_node_free(struct ast_node *node) {
         case AST_NODE_KIND_NUM:
             free(node->num);
             break;
-        case AST_NODE_KIND_OPERATOR:
+        case AST_NODE_KIND_OPER:
             ast_node_free(node->oper->op1);
             ast_node_free(node->oper->op2);
             free(node->oper);
@@ -556,24 +636,20 @@ int inter_eval(struct inter *vm, struct ast_node *node) {
 
     switch (node->kind) {
         case AST_NODE_KIND_ASSIGN:
-            printf("in assign\n");
             val = inter_eval(vm, node->assign->val);
             vm->vars[node->assign->var - 'a'] = val;
             return val;
         case AST_NODE_KIND_USE:
-            printf("in use\n");
             return vm->vars[node->use->var - 'a'];
         case AST_NODE_KIND_NUM:
-            printf("in number\n");
             return node->num->val;
-        case AST_NODE_KIND_OPERATOR:
-            printf("in oper\n");
+        case AST_NODE_KIND_OPER:
             switch (node->oper->kind) {
                 // TODO: use big num
                 case AST_OPER_KIND_ADD:
                     return inter_eval(vm, node->oper->op1) + inter_eval(vm, node->oper->op2);
                 case AST_OPER_KIND_SUB:
-                    return inter_eval(vm, node->oper->op2) - inter_eval(vm, node->oper->op1);
+                    return inter_eval(vm, node->oper->op1) - inter_eval(vm, node->oper->op2);
                 case AST_OPER_KIND_MUL:
                     return inter_eval(vm, node->oper->op1) * inter_eval(vm, node->oper->op2);
             }
@@ -590,29 +666,38 @@ void inter_print_vars(struct inter *vm) {
 }
 
 int main(int argc, char **argv) {
-    int val, c;
+    int val, car;
+    const char *msg;
     struct inter vm;
-    struct ast_node* node;
     struct charbuff* cb;
+    struct ast_parse_result result;
 
     memset(&vm, 0, sizeof(struct inter));
     cb = charbuff_new(32);
 
     printf("> ");
-    while ((c = getchar()) != EOF) {
-        if (c == '\r') continue;
-        if (c == '\n') {
-            node = ast_parse(cb->buff);
-            val = inter_eval(&vm, node);
-            printf("%d\n", val);
-            charbuff_clear(cb);
-            ast_node_free(node);
+    while ((car = getchar()) != EOF) {
+        if (car == '\r') continue;
+        if (car == '\n') {
+            if (cb->len > 0) {
+                result = ast_parse(cb->buff);
+
+                if (result.err == AST_PARSE_ERR_OK) {
+                    val = inter_eval(&vm, result.node);
+                    ast_node_free(result.node);
+                    printf("%d\n", val);
+                } else {
+                    msg = ast_parse_err_msg(result.err);
+                    if (msg != NULL) printf("%s\n", msg);
+                }
+
+                charbuff_clear(cb);
+            }
             printf("> ");
         } else {
-            charbuff_push(cb, c);
+            charbuff_push(cb, car);
         }
     }
-    printf("\nAu revoir!\n");
 
     charbuff_free(cb);
     return 0;
