686f82b438cdcb51b20ca43aa7b44447f3d80fed
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-10-08 12:04:22 -0400

    Arranger un bug et améliorer l'affichage

--- ift-2035/tp1/test.sh
+++ ift-2035/tp1/test.sh
@@ -3,20 +3,20 @@
 # dans le but de détecter des fuites de mémoire à la suite d'une erreur d'allocation.
 
 input="\
-4 4 + =x =z
-z x +
-y x +
-x x * =o
-o x * =p
-"
+4 4 + =a =b
+a b * =c =d
+d a - =e
+0 =e
+e =d
+d =b
+2 3 * 4 * 5 * 6 * 7 *"
 
 for i in `seq 1 100`; do
-    echo $input | valgrind --leak-check=full --error-exitcode=1 1> teststdout 2> teststderr ./tp1
+    echo "$input" | valgrind --leak-check=full --error-exitcode=1 1> teststdout 2> teststderr ./tp1
+    echo "=================================================================="
+    echo "Test #$i"
+    cat teststdout
     if [ $? -ne 0 ] ; then
-        echo "=================================================================="
-        echo "Test #$i"
-        cat teststdout
-        echo ""
         cat teststderr
     fi
 done
